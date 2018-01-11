5c5bcce7c8fad5ece7225566903d32c9470a0ea9
Unknown <csuwannapak@gmail.com>
2018-01-11 09:42:03 +0100

    Url Resolver JSP files and multi url link with @RequestMapping
    Résoudre problème mappage des pages jsp avec les controllers

--- src/main/java/com/csu/Application.java
+++ src/main/java/com/csu/Application.java
@@ -2,10 +2,17 @@ package com.csu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(Application.class, args);
